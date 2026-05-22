959185d2a08c6a79d6ef18f6f67f2e7d71825ad4
Samuel <cool23man23@gmail.com>
2026-05-22 16:10:47 +0200

    Added dockefile and test ci

---
+++ server/src/main.ts
@@ -0,0 +1,12 @@
+import express from "express"
+
+const app = express()
+
+app.get("ping", (req, res) => {
+    res.send("pong")
+})
+
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000")
+})
